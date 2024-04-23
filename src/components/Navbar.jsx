131156269fb329332235499e6256585b483f688d
SidakVats <sidakvats6499@gmail.com>
2024-04-23 18:33:37 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={16} height={18} />
+        <img src={appleImg} alt="Apple" width={15} height={18} />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
